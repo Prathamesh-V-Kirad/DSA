9996a01ffde1ea675e100638f25072a1b1c3e6ff
Prathamesh Vinay Kirad <kiradvprathamesh2002@gmail.com>
2024-06-26 20:34:16 +0530

    Added 1 solution for Set Matrix Zero

---
+++ Array , String/Set Matrix Zeroes/Solution_1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        ArrayList<Integer> rows = new ArrayList<Integer>();
+        ArrayList<Integer> cols = new ArrayList<Integer>();
+        for(int i = 0 ; i < matrix.length ; i++){
+            for(int j = 0 ; j < matrix[0].length;j++){
+                if(matrix[i][j] == 0){
+                    rows.add(i);
+                    cols.add(j);
+                }
+                if(cols.contains(j) || rows.contains(i)){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if(!rows.isEmpty()){
+            for(int i = 0 ; i < matrix.length ; i++){
+                for(int j = 0 ; j < matrix[0].length;j++){
+                    if(cols.contains(j) || rows.contains(i)){
+                        matrix[i][j] = 0;
+                    }
+                    if (cols.get(0) == j && rows.get(0) == i){
+                        break;
+                    }
+                }
+            }   
+        }
+    }
+}
