9abbbe4c51d138714a96d15a5bba7801df41f0b0
Prathamesh Vinay Kirad <kiradvprathamesh2002@gmail.com>
2024-07-07 15:58:28 +0530

    Modified Solution of Is Subsequence

--- Array , String/Is Subsequence/Solution_1.java
+++ Array , String/Is Subsequence/Solution_1.java
@@ -1,21 +1,14 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int i = 0 ;
-        int j = 0;
-        if(s.length() == 0){
-            return true;
-        }
-        while(j < t.length()){
-            if(s.charAt(i) == t.charAt(j)){
+        int i = 0, j = 0;
+        int m = s.length();
+        int n = t.length();
+        while (i < m && j < n) {
+            if (s.charAt(i) == t.charAt(j)) {
                 i++;
-                j++;
-            }else{
-                j++;
             }
-            if(i >= s.length()){
-                return true;
-            }
-        }
-        return false;
+            j++;
+        }  
+        return i == m;
     }
 }
