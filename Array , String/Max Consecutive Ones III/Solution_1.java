02a9de106f1ba4afb93796d7716a64c1f063fea5
Prathamesh Vinay Kirad <kiradvprathamesh2002@gmail.com>
2024-07-08 23:51:43 +0530

    Added Solution to Max Consecutive Ones

---
+++ Array , String/Max Consecutive Ones III/Solution_1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int l = 0;
+        int max = 0;
+        int cnt = 0;
+        for (int r = 0; r < nums.length; r++) {
+            if (nums[r] == 0) {
+                cnt++;
+            }
+            while (cnt > k) {
+                if (nums[l] == 0) {
+                    cnt--;
+                }
+                l++;
+            }
+            max = Math.max(max, r - l + 1);
+        }
+        return max;
+    }
+}
