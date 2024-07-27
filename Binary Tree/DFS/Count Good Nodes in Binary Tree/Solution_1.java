2d426df10b645c0dfe65e8e27d76819fcdc7809e
Prathamesh Vinay Kirad <kiradvprathamesh2002@gmail.com>
2024-07-27 23:40:53 +0530

    modified to Count Good Nodes in Binary Tree

--- Binary Tree/DFS/Count Good Nodes in Binary Tree/Solution_1.java
+++ Binary Tree/DFS/Count Good Nodes in Binary Tree/Solution_1.java
@@ -17,24 +17,16 @@ class Solution {
     public int goodNodes(TreeNode root) {
         return dfs(root, root.val);
     }
-    
-    // Depth-First Search to count good nodes
     private int dfs(TreeNode node, int maxVal) {
         if (node == null) {
             return 0;
         }
         
         int goodNodesCount = 0;
-        
-        // Check if current node is a good node
         if (node.val >= maxVal) {
             goodNodesCount = 1;
         }
-        
-        // Update max value
         maxVal = Math.max(maxVal, node.val);
-        
-        // Count good nodes in left and right subtrees
         goodNodesCount += dfs(node.left, maxVal);
         goodNodesCount += dfs(node.right, maxVal);
         
