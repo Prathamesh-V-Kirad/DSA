0170e3403462c9af92137524d36b3d6225af049e
Prathamesh Vinay Kirad <kiradvprathamesh2002@gmail.com>
2024-08-08 21:42:53 +0530

    Added 2nd Solution to Binary Tree Right Side View

---
+++ Binary Tree/BFS/Binary Tree Right Side View/Solution_2.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        DFS(root, list, 0);
+        return list;
+    }
+    public void DFS(TreeNode root, List<Integer> list, int level) {
+        if(root == null) return;
+        if(list.size() <= level) list.add(-1);
+        list.set(level, root.val);
+        DFS(root.left, list, level+1);
+        DFS(root.right, list, level+1);
+    }
+}
